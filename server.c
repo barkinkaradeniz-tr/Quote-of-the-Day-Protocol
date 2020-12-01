b8562aa7268738a7260f499e733d983307af8b4c
barkinkaradeniz-tr <barkinkaradeniz2000@gmail.com>
2020-12-01 13:27:35 +0100

    Update

--- server.c
+++ server.c
@@ -10,27 +10,40 @@
 #define MAXCHAR 512
 #define BACKLOG 10
 
+struct theText {
+    char *line;
+    int index;
+    struct theText *next;
+}theText_l;
+
 char *getText(char* filename){
 
-    char *prefix = "../";
-    char *path = malloc(MAXCHAR);
     static char line[MAXCHAR];
-    strcpy(path, prefix);
-    memcpy(path + 3, filename, sizeof(&filename));
+    int counter = 0;
 
-    FILE *fp;
-    fp = fopen(path , "r");
+    FILE *fp = fopen(filename , "r");
 
     if (fp == 0) {
         perror("Error opening the file");
-        return(-1);
+        return (-1);
     }
 
-    for (int i = 0; i < (31); i++) {
-        fgets(line, MAXCHAR, fp);
+    while (fgets(line, MAXCHAR, fp) != NULL) {
+        counter++;
     }
-
     fclose(fp);
+
+
+    FILE *fp2 = fopen(filename , "r");
+
+    if (fp2 == 0) {
+        perror("Error opening the file");
+        return (-1);
+    }
+    for (int i = 0; i < (rand() % counter); i++) {
+        fgets(line, MAXCHAR, fp2);
+    }
+    fclose(fp2);
 
 
     return line;
@@ -39,13 +52,13 @@ char *getText(char* filename){
 int main(int argc, char *argv[] ){
 
     struct addrinfo hints, *servinfo;
-    int sock_fd, new_fd;
     struct sockaddr_storage their_addr;
-    socklen_t addr_size;
     char *msg = getText(argv[1]);
+    int sock_fd, new_fd;
     int len, bytes_sent;
+
+    socklen_t addr_size;
     len = strlen(msg);
-    printf("%s", msg);
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
@@ -59,7 +72,13 @@ int main(int argc, char *argv[] ){
 
     addr_size = sizeof(their_addr);
     new_fd = accept(sock_fd, (struct sockaddr *)&their_addr, &addr_size);
-    bytes_sent = send(new_fd, msg, len, 0);
+
+    for (int i = 0; i < len; i++) {
+        bytes_sent = send(new_fd, &msg[i], 1, 0);
+        if (bytes_sent == 0) {
+            perror("Byte failed to send.");
+        }
+    }
 
     close(new_fd);
     close(sock_fd);
